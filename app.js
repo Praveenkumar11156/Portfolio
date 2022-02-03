218d99403113caa29cd88522c75e58d4ccbb2aa9
Praveenkumar11156 <91111675+Praveenkumar11156@users.noreply.github.com>
2022-02-03 11:31:50 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const body = document.querySelector('body'),
 sidebar = body.querySelector('nav'),
 toggle = body.querySelector(".toggle"),
-searchBtn = body.querySelector(".search-box"),
+//searchBtn = body.querySelector(".search-box"),
 modeSwitch = body.querySelector(".toggle-switch"),
 modeText = body.querySelector(".mode-text");
 
@@ -21,9 +21,9 @@ toggle.addEventListener("click" , () =>{
     sidebar.classList.toggle("close");
 })
 
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
+//searchBtn.addEventListener("click" , () =>{
+  //  sidebar.classList.remove("close");
+//})
 
 modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
